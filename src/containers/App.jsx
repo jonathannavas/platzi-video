0a5bc97de2477349441b20ef6875446a0fa9afb2
Jonathan Navas <jonathan.navas9@hotmail.com>
2021-06-30 22:56:24 -0500

    Proyecto finalizado

---
+++ src/containers/App.jsx
@@ -0,0 +1,59 @@
+import React, { useEffect, useState } from 'react';
+import '../assets/styles/App.scss';
+import Header from '../components/Header';
+import Search from '../components/Search';
+import Categories from '../components/Categories';
+import Carousel from '../components/Carousel';
+import CarouselItem from '../components/CarouselItem';
+import Footer from '../components/Footer';
+
+import useInitialState from '../hooks/useInitialState';
+const API = 'http://localhost:3000/initalState';
+
+const App = () => {
+
+
+    const initialState = useInitialState(API)
+
+    return(
+        <div className="App">
+            <Header />
+            <Search />
+
+            {
+                initialState.mylist.length > 0 && (
+                    <Categories title='Mi Lista'>
+                        <Carousel>
+                            <CarouselItem />
+                        </Carousel>
+                    </Categories>
+                )
+            }
+
+            <Categories title='Tendencias'>
+                <Carousel>
+                    {
+                        initialState.trends.map( item => {
+                           return <CarouselItem  key={item.id} {...item} />
+                        })
+                    }
+                </Carousel>
+            </Categories>
+
+            <Categories title='Originales de Platzi Video'>
+                <Carousel>
+                    {
+                        initialState.originals.map( item => {
+                            return <CarouselItem  key={item.id} {...item} />
+                         })
+                    }
+                </Carousel>
+            </Categories>
+
+            <Footer />
+        </div>
+    );
+}
+
+
+export default App;
--- webpack.config.js
+++ webpack.config.js
@@ -37,6 +37,16 @@ module.exports = {
                     'css-loader',
                     'sass-loader'
                 ]
+            }, {
+                test: /\.(png|gif|jpg)$/,
+                use: [
+                    {
+                        'loader': 'file-loader',
+                        options: {
+                            name: 'assets/[hash].[ext]'
+                        }
+                    }
+                ]
             }
         ]
     },
